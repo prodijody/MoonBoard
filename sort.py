7ef7174e36d706d127ecb72f9f7b62f560935e0b
Sourcery AI <>
2023-11-29 15:04:10 +0000

    'Refactored by Sourcery'

--- sort.py
+++ sort.py
@@ -6,16 +6,11 @@ def sort_routes(grade, sort):
         raw_data = json.load(f)
 
     data = raw_data["data"]
-    problem_list = []
-
-    for i in data:
-        if i["grade"] == grade.upper():
-            problem_list.append(i)
-
-    if sort == "repeats":
-        problem_list = sorted(problem_list, key=lambda x: x["repeats"], reverse=True)
-
+    problem_list = [i for i in data if i["grade"] == grade.upper()]
     if sort == "rating":
         problem_list = sorted(problem_list, key=lambda x: x["userRating"], reverse=True)
+
+    elif sort == "repeats":
+        problem_list = sorted(problem_list, key=lambda x: x["repeats"], reverse=True)
 
     return problem_list
